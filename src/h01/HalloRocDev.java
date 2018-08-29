dc3520f504e106b26cb0ab1e3295143ceb17be52
melihpoh4o <42801398+melihpoh4o@users.noreply.github.com>
2018-08-29 11:41:17 +0200

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo Iedereen ");
+        System.out.println("Hallo Iedereen ik ben Melih ");
     }
 }
